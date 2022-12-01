a13fa50f756b60c572218203b0b5ab70775ef63f
atakahashi <atakahashi@cbt-s.com>
2022-12-01 11:48:39 +0900

    test

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html --no-scripts
-composer update --no-dev --working-dir=/var/www/html
+#composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 #php artisan config:cache
@@ -11,6 +10,8 @@ echo "Caching routes..."
 #php artisan route:cache
 
 echo "composer dump..."
+composer install --no-scripts
+composer update --no-dev
 composer dump-autoload -o
 
 echo "Running migrations..."
